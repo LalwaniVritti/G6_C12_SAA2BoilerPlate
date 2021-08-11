ee0e2f105d5e1ce1244f838d86595b9ec5115ae9
LalwaniVritti <88480121+LalwaniVritti@users.noreply.github.com>
2021-08-11 14:20:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// create variable
+
+function draw() {
+  background("lightblue");
+
+  //Write if condition for space and assign random number to variable
+
+
+  //print value and string on canvas using text function and string concatenation 
+
+  textSize(20);
+
+  
+  
+}
